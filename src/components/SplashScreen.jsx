eebedd227df8901638bca8bd338dd4a8abd5c6ef
Simo <mohamedelhajri12345@gmail.com>
2026-08-12 01:41:39 +0100

    Add Noor animated splash screen component

---
+++ src/components/SplashScreen.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function SplashScreen(){
+  return (
+    <div className="splash-screen">
+      <div className="splash-logo">🕋</div>
+      <h1 className="splash-title">Noor</h1>
+      <p>نورٌ يرافقك في طريق الإيمان</p>
+      <div className="progress-bar">
+        <div className="progress-fill"></div>
+      </div>
+    </div>
+  );
+}
