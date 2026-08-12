22bf1d424d104285b31863efcf9ce1c6b2ab3b66
Simo <mohamedelhajri12345@gmail.com>
2026-08-12 01:47:26 +0100

    Add prayer service foundation

---
+++ src/services/prayerService.js
@@ -0,0 +1,12 @@
+// Prayer times service foundation
+// Ready for location and prayer calculation integration.
+
+export const getPrayerTimes = async () => {
+  return {
+    fajr: null,
+    dhuhr: null,
+    asr: null,
+    maghrib: null,
+    isha: null,
+  };
+};
