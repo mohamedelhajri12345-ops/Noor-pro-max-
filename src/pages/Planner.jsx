041355a85980847563894ec0c44a1cbec625e1bb
Simo <mohamedelhajri12345@gmail.com>
2026-08-12 01:44:43 +0100

    Add Islamic planner page component

---
+++ src/pages/Planner.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Planner(){
+  return <section className="page"><h2>📝 المفكرة الإسلامية</h2><p>مكان لكتابة الأعمال اليومية والأهداف الروحية.</p></section>;
+}
