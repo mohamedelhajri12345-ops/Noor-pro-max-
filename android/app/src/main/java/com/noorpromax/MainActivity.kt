dbbbd720879b4e91b87f0a1093f6447c592edbad
Simo <mohamedelhajri12345@gmail.com>
2026-08-12 12:12:45 +0100

    feat(ads): register native Start.io Capacitor bridge

--- android/app/src/main/java/com/noorpromax/MainActivity.kt
+++ android/app/src/main/java/com/noorpromax/MainActivity.kt
@@ -1,3 +1,11 @@
 package com.noorpromax
+
+import android.os.Bundle
 import com.getcapacitor.BridgeActivity
-class MainActivity : BridgeActivity()
+
+class MainActivity : BridgeActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        registerPlugin(StartIoPlugin::class.java)
+        super.onCreate(savedInstanceState)
+    }
+}
