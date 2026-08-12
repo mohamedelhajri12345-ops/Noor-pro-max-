ca2ef563404fd5fd56972a4c351f3c91463ff023
Simo <mohamedelhajri12345@gmail.com>
2026-08-12 02:00:40 +0100

    Add Noor Banner ad component for Start.io integration

---
+++ src/components/ads/NoorBannerAd.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function NoorBannerAd() {
+  return (
+    <div className="noor-banner-ad" aria-label="Advertisement">
+      {/* Start.io Banner Ad container */}
+      <div id="startio-banner" />
+    </div>
+  );
+}
