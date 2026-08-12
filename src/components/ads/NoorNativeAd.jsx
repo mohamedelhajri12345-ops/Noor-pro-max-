74d22b87ce24e34d3f37b14d2de102e8fa2e77fd
Simo <mohamedelhajri12345@gmail.com>
2026-08-12 02:00:46 +0100

    Add Noor Native ad component for Start.io integration

---
+++ src/components/ads/NoorNativeAd.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function NoorNativeAd() {
+  return (
+    <div className="noor-native-ad" aria-label="Advertisement">
+      {/* Start.io Native Ad container */}
+      <div id="startio-native" />
+    </div>
+  );
+}
