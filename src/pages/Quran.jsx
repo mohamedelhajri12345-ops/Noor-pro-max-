45c5344bbe3ba2e8742b5f630c92ca4db78e78e3
Simo <mohamedelhajri12345@gmail.com>
2026-08-12 01:42:31 +0100

    Add Quran page component

---
+++ src/pages/Quran.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Quran(){
+ return <section><h2>📖 القرآن الكريم</h2><p>قراءة القرآن والاستماع بتجربة متصلة.</p><button>تشغيل التلاوة</button></section>;
+}
