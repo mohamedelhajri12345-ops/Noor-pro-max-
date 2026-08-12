137ab3e832d22f1107bb18b349ee878f97628ae3
Simo <mohamedelhajri12345@gmail.com>
2026-08-12 01:48:10 +0100

    Add Adhan configuration

---
+++ src/config/adhanConfig.js
@@ -0,0 +1,14 @@
+// Adhan configuration for Noor
+
+export const adhanConfig = {
+  sounds: [
+    'adhan1',
+    'adhan2',
+    'adhan3'
+  ],
+  reminders: [
+    'اقرأ حزب اليوم',
+    'أذكار الصباح',
+    'أذكار المساء'
+  ],
+};
