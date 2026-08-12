960c653c6d5f4d109d671c850d4fbca233b22527
Simo <mohamedelhajri12345@gmail.com>
2026-08-12 01:51:41 +0100

    Add Android prayer alarm scheduler foundation

---
+++ android/AdhanScheduler.kt
@@ -0,0 +1,12 @@
+package com.noor.app
+
+/**
+ * Scheduler foundation for prayer alarms.
+ * Will connect prayer calculation results with Android alarms.
+ */
+class AdhanScheduler {
+    fun schedulePrayerNotifications() {
+        // TODO: Use Android AlarmManager/WorkManager.
+        // TODO: Schedule Fajr, Dhuhr, Asr, Maghrib and Isha.
+    }
+}
