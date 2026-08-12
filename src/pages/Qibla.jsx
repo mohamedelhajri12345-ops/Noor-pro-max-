b40f2d7b75efc9f5ffa9c91292cbe71afedfc409
Simo <mohamedelhajri12345@gmail.com>
2026-08-12 01:44:35 +0100

    Add Qibla page component

---
+++ src/pages/Qibla.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Qibla(){
+  return <section className="page"><h2>🧭 القبلة</h2><p>تحديد اتجاه القبلة سيتم ربطه بالبوصلة والموقع.</p></section>;
+}
